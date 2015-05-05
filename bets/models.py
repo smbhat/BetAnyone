63a396392126579f3c3013e277342fc1173d11d8
Suraj Bhat <smbhat@nat-oitwireless-inside-vapornet100-b-4365.Princeton.EDU>
2015-05-04 21:41:48 -0400

    begin bet model

--- bets/models.py
+++ bets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	netid = models.CharField(max_length = 50)
+	balance = models.FloatField(default = 0)
+	committed = models.FloatField(default = 0)
+
+
+
+class Bet(models.Model):
+	description = models.CharField(max_length = 1000)
+	value = models.IntegerField(default = 0)
+	category = models.CharField(max_length = 50)
+	status = models.BooleanField(default = False) # True if taken, False if open
+
+	creater = models.ForeignKey(User)
+	taker = models.ForeignKey(User)
+	arbitrator = models.ForeignKey(User)
