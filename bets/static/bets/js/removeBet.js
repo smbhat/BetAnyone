8ec41a47c0ce3e19ffb8e1b7745098fac1f88432
Jeffrey <jeffreyyan8@gmail.com>
2015-05-06 12:32:21 -0400

    delete button and index

---
+++ bets/static/bets/js/removeBet.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.list-group-item').each(function(index, element) {
+    $(element).find('button').click(function(e) {
+      e.preventDefault();
+      console.log(this.id);
+      var betId = this.id.substring(4);
+      console.log(betId);
+      var data = { betId: betId } ;
+      $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: '../deleteBets/' + betId,
+        success: function (response) {
+          console.log(response);
+          $(element).remove();
+        },
+        error: function (response) {
+        }
+      });
+    });
+  });
+});
