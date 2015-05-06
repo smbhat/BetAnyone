bf479616b9447a153abd0afeaeeb317fca6642df
Suraj Bhat <smbhat@nat-oitwireless-inside-vapornet100-d-3829.Princeton.EDU>
2015-05-06 12:26:50 -0400

    forms now working

--- bets/views.py
+++ bets/views.py
@@ -9,9 +9,48 @@ NETID = 'smbhat'
 def index(request):
 	return render(request, 'bets/index.html')
 
+# Helper function to verify whether the input string represents a float. Return
+# True if it does represent a float, False otherwise.
+def verifiedNumber(num):
+	try:
+		float(num)
+		return True
+	except ValueError:
+		return False
 
 def dashboard(request):
 	u = User.objects.get(netid = NETID)
+
+
+	if request.method == 'POST':
+		if 'bet_name' in request.POST:
+			n = request.POST['bet_name']
+			amt = request.POST['bet_amt']
+			d = request.POST['bet_desc']
+			if verifiedNumber(amt):
+				b = Bet(name = n, value = float(amt), description = d, category = '', creator = u)
+				b.save()
+				print 'New bet made.'
+			else:
+				print 'Not a valid amount.'
+
+		elif 'netid' in request.POST:
+			netid = request.POST['netid']
+			if u.isFriend(netid) == False:
+				u.addFriend(netid)
+				u.save()
+				print 'New friend added.'
+			else:
+				print 'Already friends.'
+		
+		elif 'card_amt' in request.POST:
+			paymentamt = request.POST['card_amt']
+			if verifiedNumber(paymentamt):
+				u.addBalance(float(paymentamt))
+				u.save()
+				print 'New payment made.'
+			else:
+				print 'Not a valid amount.'
 
 	balance = '$' + format(u.balance, '.2f')
 	committed = '$' + format(u.committed, '.2f')
@@ -26,8 +65,15 @@ def dashboard(request):
 
 def betpage(request, cbet):
 
-	bet = Bet.objects.get(id = cbet)
 
-	context = {'title': bet.name, 'description': bet.description, 'value': bet.value, 'creator': bet.creator.netid, 'arbitrator': bet.arbitrator.netid, 'taker': bet.taker.netid}
+	bet = Bet.objects.get(id = cbet)
+	arbitrator = 'Not Yet Taken'
+	taker = 'Not Yet Taken'
+	if bet.status == True:
+		arbitrator = bet.arbitrator.netid
+		taker = bet.taker.netid
+
+
+	context = {'title': bet.name, 'description': bet.description, 'value': bet.value, 'creator': bet.creator.netid, 'arbitrator': arbitrator, 'taker': taker}
 
 	return render(request, 'bets/betpage.html', context)
