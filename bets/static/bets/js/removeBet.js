effdffb49a90e9cd25b358d670ced91670b5a396
Jeffrey <jeffreyyan8@gmail.com>
2015-05-06 20:38:16 -0400

    delete fixed

--- bets/static/bets/js/removeBet.js
+++ bets/static/bets/js/removeBet.js
@@ -1,18 +1,42 @@
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (settings.type == 'POST' || settings.type == 'PUT' || settings.type == 'DELETE') {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    }
+});
+
 $(document).ready(function() {
   $('.list-group-item').each(function(index, element) {
     $(element).find('.deleteButton').click(function(e) {
-      console.log('----here2');
-
       e.preventDefault();
       console.log(this.id);
       var betId = this.id.substring(5);
       console.log(betId);
-      var data = { betId: betId } ;
+      var data = {betId: betId};
       $.ajax({
         type: 'POST',
-        data: JSON.stringify(data),
+        data: {betId: betId, csrfmiddlewaretoken: '{{ csrf_token }}'},
         contentType: 'application/json',
-        url: '../deletebet/' + betId,
+        url: '/deletebet/' + betId + '/',
         success: function (response) {
           console.log(response);
           $(element).remove();
