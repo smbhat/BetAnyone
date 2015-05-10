3b925b93966fed38639814fafb426151e9fab83e
Suraj Bhat <smbhat@nat-oitwireless-inside-vapornet100-d-3829.Princeton.EDU>
2015-05-09 20:26:32 -0400

    added student population script

---
+++ populate_users.py
@@ -0,0 +1,25 @@
+import json
+import sys
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CurveIt.settings')
+import django
+django.setup()
+from bets.models import Player
+
+def main():
+	users = []
+	Player.objects.all().delete()
+	jsonFile = open("students.json")
+	studentList = json.loads(jsonFile.readline())
+	for student in studentList:
+		studentDict = {}
+		name = student[0]
+		year = student[1]
+		email = student[2]
+		netid = email[0:email.index("@")]
+		Users.append(Student(netid=netid))
+
+	for u in users:
+		u.save()
+
+main()
